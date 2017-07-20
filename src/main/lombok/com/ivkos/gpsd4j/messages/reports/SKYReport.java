69749fbf203044294f5eed9fa7716316cb9da1cb
Ivaylo Stoyanov <me@ivkos.com>
2017-07-20 12:42:33 +0300

    SKYReport: Add javadocs and friendlier field names

--- src/main/lombok/com/ivkos/gpsd4j/messages/reports/SKYReport.java
+++ src/main/lombok/com/ivkos/gpsd4j/messages/reports/SKYReport.java
@@ -16,6 +16,7 @@
 
 package com.ivkos.gpsd4j.messages.reports;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.ivkos.gpsd4j.messages.GpsdMessage;
 import com.ivkos.gpsd4j.messages.Satellite;
 import lombok.EqualsAndHashCode;
@@ -25,6 +26,21 @@ import lombok.ToString;
 import java.time.LocalDateTime;
 import java.util.List;
 
+import static java.util.Collections.emptyList;
+import static java.util.Collections.unmodifiableList;
+import static lombok.AccessLevel.NONE;
+
+/**
+ * A SKY object reports a sky view of the GPS satellite positions. If there is no GPS device available, or no skyview
+ * has been reported yet, only the "class" field will reliably be present.
+ * <p>
+ * Many devices compute dilution of precision factors but do not include them in their reports. Many that do report DOPs
+ * report only HDOP, two-dimensional circular error. gpsd always passes through whatever the device actually reports,
+ * then attempts to fill in other DOPs by calculating the appropriate determinants in a covariance matrix based on the
+ * satellite view. DOPs may be missing if some of these determinants are singular. It can even happen that the device
+ * reports an error estimate in meters when the corresponding DOP is unavailable; some devices use more sophisticated
+ * error modeling than the covariance calculation.
+ */
 @Getter
 @ToString
 @EqualsAndHashCode(callSuper = false)
@@ -32,14 +48,72 @@ public class SKYReport extends GpsdMessage
 {
    public static final String CLASS = "SKY";
 
+   /**
+    * @return Name of originating device
+    */
    private String device;
 
    private LocalDateTime time;
-   private Double tdop;
 
+   /**
+    * @return Time dilution of precision, a dimensionless factor which should be multiplied by a base UERE to get an
+    * error estimate.
+    */
+   @JsonProperty("tdop")
+   private Double timeDOP;
+
+   @Getter(NONE)
    private List<Satellite> satellites;
 
-   private Double xdop, ydop, vdop, hdop, pdop, gdop;
+   /**
+    * @return Longitudinal dilution of precision, a dimensionless factor which should be multiplied by a base UERE to
+    * get an error estimate.
+    */
+   @JsonProperty("xdop")
+   private Double longitudeDOP;
+
+   /**
+    * @return Latitudinal dilution of precision, a dimensionless factor which should be multiplied by a base UERE to get
+    * an error estimate.
+    */
+   @JsonProperty("ydop")
+   private Double latitudeDOP;
+
+   /**
+    * @return Altitude dilution of precision, a dimensionless factor which should be multiplied by a base UERE to get an
+    * error estimate.
+    */
+   @JsonProperty("vdop")
+   private Double altitudeDOP;
+
+   /**
+    * @return Horizontal dilution of precision, a dimensionless factor which should be multiplied by a base UERE to get
+    * a circular error estimate.
+    */
+   @JsonProperty("hdop")
+   private Double horizontalDOP;
+
+   /**
+    * @return Spherical dilution of precision, a dimensionless factor which should be multiplied by a base UERE to get
+    * an error estimate.
+    */
+   @JsonProperty("pdop")
+   private Double sphericalDOP;
+
+   /**
+    * @return Hyperspherical dilution of precision, a dimensionless factor which should be multiplied by a base UERE to
+    * get an error estimate.
+    */
+   @JsonProperty("gdop")
+   private Double hypersphericalDOP;
+
+   /**
+    * @return List of satellite objects in skyview
+    */
+   public List<Satellite> getSatellites()
+   {
+      return satellites != null ? unmodifiableList(satellites) : emptyList();
+   }
 
    @Override
    public String getGpsdClass()
